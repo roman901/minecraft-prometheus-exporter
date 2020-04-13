cb9ed408ae1f4a46a064fa2ec6222a41e3e2d9af
BrilZliaN <brilzlian@ya.ru>
2020-04-14 02:39:16 +0300

    Catch collector errors and log them if needed

--- src/main/kotlin/space/epix/exporter/core/EpixPrometheusCollector.kt
+++ src/main/kotlin/space/epix/exporter/core/EpixPrometheusCollector.kt
@@ -1,14 +1,17 @@
 package space.epix.exporter.core
 
 import io.prometheus.client.Collector
-import io.prometheus.client.GaugeMetricFamily
 import org.reflections.Reflections
+import space.epix.exporter.PaperPrometheusExporter
 import java.util.*
+import java.util.logging.Level
 import kotlin.reflect.full.findAnnotation
 
 
 class EpixPrometheusCollector(private var runtime: CollectibleType) : Collector() {
     private val suppliers = ArrayList<CollectibleSupplier>()
+    private val logFailures = PaperPrometheusExporter.instance.config
+            .getBoolean("log.collector-failures", false)
 
     init {
         val reflections = Reflections("space.epix.exporter.suppliers")
@@ -25,10 +28,14 @@ class EpixPrometheusCollector(private var runtime: CollectibleType) : Collector(
 
     override fun collect(): List<MetricFamilySamples> {
         val metrics = ArrayList<MetricFamilySamples>()
-        suppliers.forEach {
-            val gaugeMetricFamily = it.collect()
-            if (gaugeMetricFamily != null) {
-                metrics.add(gaugeMetricFamily)
+        suppliers.forEach { supplier ->
+            try {
+                supplier.collect()?.let { metrics.add(it) }
+            } catch (throwable: Throwable) {
+                if (logFailures) {
+                    PaperPrometheusExporter.instance.logger.log(Level.SEVERE,
+                            "Can't collect ${supplier.javaClass.simpleName}", throwable)
+                }
             }
         }
         return metrics
