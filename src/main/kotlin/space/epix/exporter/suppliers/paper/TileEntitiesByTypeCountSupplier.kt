fa12ce47ecdd5a8bf5e59a4ab3bb30b259343521
BrilZliaN <brilzlian@ya.ru>
2020-04-14 02:39:41 +0300

    Fix tile entities suppliers across different Paper versions

--- src/main/kotlin/space/epix/exporter/suppliers/paper/TileEntitiesByTypeCountSupplier.kt
+++ src/main/kotlin/space/epix/exporter/suppliers/paper/TileEntitiesByTypeCountSupplier.kt
@@ -6,18 +6,19 @@ import space.epix.exporter.core.Collectible
 import space.epix.exporter.core.CollectibleSupplier
 import space.epix.exporter.core.CollectibleType
 
-
+@Collectible(type = CollectibleType.PAPER)
 class TileEntitiesByTypeCountSupplier : CollectibleSupplier {
-    override fun build() { }
+    override fun build() {}
 
     override fun collect(): GaugeMetricFamily? {
-        val gauge = GaugeMetricFamily("mc_tile_entities", "Tile entities count", listOf("world", "type"))
+        val gauge = GaugeMetricFamily("mc_tile_entities_type", "Tile entities by type count", listOf("world", "type"))
         Bukkit.getWorlds().forEach { world ->
-            world.loadedChunks.forEach {chunk ->  
-                chunk.tileEntities.groupBy { it.type }.forEach {
-                    gauge.addMetric(listOf(world.name, it.key.name), it.value.size.toDouble())
-                }
-            }
+            world.loadedChunks
+                    .flatMap { it.tileEntities.asIterable() }
+                    .groupBy { it.type }
+                    .forEach {
+                        gauge.addMetric(listOf(world.name, it.key.name), it.value.size.toDouble())
+                    }
         }
         return gauge
     }
--- src/main/kotlin/space/epix/exporter/suppliers/paper/TileEntitiesCountSupplier.kt
+++ src/main/kotlin/space/epix/exporter/suppliers/paper/TileEntitiesCountSupplier.kt
@@ -7,13 +7,19 @@ import space.epix.exporter.core.CollectibleSupplier
 import space.epix.exporter.core.CollectibleType
 
 
-@Collectible(type=CollectibleType.PAPER) class TileEntitiesCountSupplier : CollectibleSupplier {
-    override fun build() { }
+@Collectible(type = CollectibleType.PAPER)
+class TileEntitiesCountSupplier : CollectibleSupplier {
+    override fun build() {}
 
     override fun collect(): GaugeMetricFamily? {
         val gauge = GaugeMetricFamily("mc_tile_entities", "Tile entities count", listOf("world"))
-        Bukkit.getWorlds().forEach {
-            gauge.addMetric(listOf(it.name), it.tileEntityCount.toDouble())
+        Bukkit.getWorlds().forEach { world ->
+            val tileEntityCount = try {
+                world.tileEntityCount.toDouble()
+            } catch (throwable: Throwable) {
+                world.loadedChunks.map { it.tileEntities.size }.sum().toDouble()
+            }
+            gauge.addMetric(listOf(world.name), tileEntityCount)
         }
         return gauge
     }
